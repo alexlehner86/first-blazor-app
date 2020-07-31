8149731cab6f789bca2b944716428d141ef0b91f
Alexander Lehner <xander_lehner@gmx.at>
2020-07-31 09:29:08 +0200

    first steps

---
+++ Client/helpers/StringUtilities.cs
@@ -0,0 +1,8 @@
+namespace firstBlazorApp.Client.Helpers
+{
+    public class StringUtilities
+    {
+        public static string CustomToLower(string value) => value.ToLower();
+        public static string CustomToUpper(string value) => value.ToUpper();
+    }
+}
---
+++ Shared/Entities/Movie.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace firstBlazorApp.Shared.Entities
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
